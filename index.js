df3d277a942aa5a50657fdc2eaa426090156b903
rachmann0 <reecedarby1889@gmail.com>
2021-12-09 16:25:40 +0700

    fix wrong sql proc

--- index.js
+++ index.js
@@ -3,22 +3,18 @@ const express = require('express');
 const { authRoutes, profileRoutes } = require('./src/routes');
 const app = express();
 const PORT = process.env.PORT || 2003;
-const mysql = require("./src/connections/db")
-const cors = require("cors")
-
-
+const mysql = require('./src/connections/db');
+const cors = require('cors');
 
 app.use(cors());
 // ! body parse
-
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // ! routes
 app.use('/', require('./src/routes/rootRoutes'));
-app.use('/auth', authRoutes)
-app.use('/profile', profileRoutes)
+app.use('/auth', authRoutes);
+app.use('/profile', profileRoutes);
 app.use('/raw_material', require('./src/routes/rawMaterialRoutes'));
 app.use('/product', require('./src/routes/productRoutes'));
 app.use('/auth', require('./src/routes/authRoutes'));
