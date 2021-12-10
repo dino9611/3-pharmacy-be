cd058d418cf96ff12d5193f6d92464c5ebcbcf4d
Gvrarnzs <arianzas1997@gmail.com>
2021-12-10 09:58:27 +0700

    PJT-9 Merge Develop-be

--- src/helpers/verifyToken.js
+++ src/helpers/verifyToken.js
@@ -3,24 +3,14 @@ const jwt = require("jsonwebtoken")
 
 
 module.exports = {
-<<<<<<< HEAD
     verifyEmailToken : (req,res, next) => {
         // const {token} = req.body
         const token = req.token
-=======
-    verifyEmailToken: (req, res, next) => {
-        // const token = req.token
-        const { token } = req.body
->>>>>>> develop-be
         const key = process.env.JWT_KEY
         jwt.verify(token, key, (err, decoded) => {
             if (err) {
                 console.log(err)
-<<<<<<< HEAD
                 return res.status(401).send({message: "User Unauthorized"})
-=======
-                return res.status(401).send({ message: "user unauthorized" })
->>>>>>> develop-be
             }
             console.log(decoded)
             req.user = decoded
