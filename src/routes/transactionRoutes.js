2b7f76ab85cf1d3fd41665708a1a64e0e6e09938
Hilmawan Zaky <hilmawanzaky57@gmail.com>
2021-12-18 15:10:41 +0700

    PJT-28 improve add to and delete from cart

--- src/routes/transactionRoutes.js
+++ src/routes/transactionRoutes.js
@@ -1,8 +1,9 @@
-const { addToCart, deleteFromCart } = require('../controllers/transactionControllers');
+const { addToCart, deleteFromCart, getCart } = require('../controllers/transactionControllers');
 
 const router = require('express').Router();
 
 router.post('/addtocart/:user_id', addToCart)
 router.delete('/deletefromcart/:user_id/:product_id', deleteFromCart)
+router.get('/getcart/:user_id', getCart)
 
 module.exports = router
