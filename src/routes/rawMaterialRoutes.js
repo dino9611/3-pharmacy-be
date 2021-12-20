8b564df9367e866547264b06d3708dec7c7357fc
rachmann0 <reecedarby1889@gmail.com>
2021-12-20 22:29:56 +0700

    verifyAdmin for productRoutes

--- src/routes/rawMaterialRoutes.js
+++ src/routes/rawMaterialRoutes.js
@@ -5,19 +5,18 @@ const {
   readRawMaterialRecord,
   updateRawMaterial,
 } = require('../controllers/rawMaterialControllers');
-// const {} = require('../helpers/verifyJWT');
+const { verifyAccessToken } = require('../helpers/verifyToken');
+const { verifyAdmin } = require('../middlewares/verifyAdmin');
 
 const route = express.Router();
 
-// route.use(someAdminVerifierMiddleware())
+route.use(verifyAccessToken);
+route.use(verifyAdmin);
 
-// ? admin request
+// ? all admin request
 route.post('/', createRawMaterial);
-// ? admin request
 route.get('/record', readRawMaterialRecord);
-// ? admin request
 route.get('/:raw_material_id?', readRawMaterial);
-// ? admin request
 route.patch('/:raw_material_id', updateRawMaterial);
 
 module.exports = route;
--- src/routes/statsRoutes.js
+++ src/routes/statsRoutes.js
@@ -3,8 +3,13 @@ const {
   readRevenue,
   readPotentialRevenue,
 } = require('../controllers/statsControllers');
+const { verifyAccessToken } = require('../helpers/verifyToken');
+const { verifyAdmin } = require('../middlewares/verifyAdmin');
 
 const route = express.Router();
+
+route.use(verifyAccessToken);
+route.use(verifyAdmin);
 
 // ? admin request
 route.get('/revenue', readRevenue);
