98a8950722b632126a16ac06065bc94e573871de
rachmann0 <reecedarby1889@gmail.com>
2021-12-21 23:19:06 +0700

    add yearly revenue

--- src/controllers/statsControllers.js
+++ src/controllers/statsControllers.js
@@ -1,23 +1,36 @@
 const pool = require('../connections/db');
 
 // ! READ
+// ? revenue
 exports.readRevenue = async (req, res) => {
   let { yearMonthStart, yearMonthEnd } = req.query;
 
-  let sql;
+  let sql, parameters;
   try {
-    sql = `
-		SELECT CAST(SUM(totalPrice) AS SIGNED) totalRevenueRp, MONTH(checkedOutAt) month, YEAR(checkedOutAt) year
-		FROM 3_pharmacy.order
-		WHERE checkedOutAt BETWEEN ? AND LAST_DAY(?)
-		AND !(status = 'checkout' OR status = 'paymentRej' OR status IS NULL)
-		GROUP BY month, year
-		ORDER BY checkedOutAt ASC;`;
-    let [result] = await pool.query(sql, [yearMonthStart, yearMonthEnd]);
+    console.log(req.params.time);
+    if (req.params.time === 'yearly') {
+      sql = `
+      SELECT CAST(SUM(totalPrice) AS SIGNED) totalRevenueRp, YEAR(checkedOutAt) year
+      FROM 3_pharmacy.order
+      WHERE !(status = 'checkout' OR status = 'paymentRej' OR status IS NULL)
+      GROUP BY year
+      ORDER BY checkedOutAt ASC;`;
+    } else if (req.params.time === undefined) {
+      sql = `
+      SELECT CAST(SUM(totalPrice) AS SIGNED) totalRevenueRp, MONTH(checkedOutAt) month, YEAR(checkedOutAt) year
+      FROM 3_pharmacy.order
+      WHERE checkedOutAt BETWEEN ? AND LAST_DAY(?)
+      AND !(status = 'checkout' OR status = 'paymentRej' OR status IS NULL)
+      GROUP BY month, year
+      ORDER BY checkedOutAt ASC;`;
+      parameters = [yearMonthStart, yearMonthEnd];
+    } else return res.status(404).json({ message: 'Not Found' });
+    let [result] = await pool.query(sql, parameters);
 
     res.status(200).json(result);
   } catch (error) {
     console.log(error);
+    res.status(500).send({ message: 'server error' });
   }
 };
 exports.readPotentialRevenue = async (req, res) => {
@@ -25,16 +38,54 @@ exports.readPotentialRevenue = async (req, res) => {
 
   let sql;
   try {
-    sql = `
-		SELECT CAST(SUM(totalPrice) AS SIGNED) totalPotentialRevenueRp, MONTH(checkedOutAt) month, YEAR(checkedOutAt) year
-		FROM 3_pharmacy.order
-		WHERE checkedOutAt BETWEEN ? AND LAST_DAY(?)
-		GROUP BY month, year
-		ORDER BY checkedOutAt ASC;`;
+    if (req.params.time === 'yearly') {
+      sql = `
+      SELECT CAST(SUM(totalPrice) AS SIGNED) totalPotentialRevenueRp, YEAR(checkedOutAt) year
+      FROM 3_pharmacy.order
+      GROUP BY year
+      ORDER BY checkedOutAt ASC;`;
+    } else if (req.params.time === undefined) {
+      sql = `
+      SELECT CAST(SUM(totalPrice) AS SIGNED) totalPotentialRevenueRp, MONTH(checkedOutAt) month, YEAR(checkedOutAt) year
+      FROM 3_pharmacy.order
+      WHERE checkedOutAt BETWEEN ? AND LAST_DAY(?)
+      GROUP BY month, year
+      ORDER BY checkedOutAt ASC;`;
+    } else return res.status(404).json({ message: 'Not Found' });
     let [result] = await pool.query(sql, [yearMonthStart, yearMonthEnd]);
 
     res.status(200).json(result);
   } catch (error) {
     console.log(error);
+    res.status(500).send({ message: 'server error' });
+  }
+};
+
+// ? sales report
+exports.readOrders = async (req, res) => {
+  let { status } = req.body;
+
+  let sql, parameters;
+  try {
+    sql = `
+    SELECT *
+    FROM 3_pharmacy.order
+    WHERE status IN (?);`;
+    const allStatus = [
+      'checkout',
+      'paymentAcc',
+      'paymentRej',
+      'processing',
+      'otw',
+      'delivered',
+    ];
+    status = status.filter((el) => allStatus.includes(el));
+    parameters = status;
+    const [result] = await pool.query(sql, parameters);
+
+    res.status(200).json(result);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ message: 'server error' });
   }
 };
--- src/routes/statsRoutes.js
+++ src/routes/statsRoutes.js
@@ -2,17 +2,20 @@ const express = require('express');
 const {
   readRevenue,
   readPotentialRevenue,
+  readOrders,
 } = require('../controllers/statsControllers');
 const { verifyAccessToken } = require('../helpers/verifyToken');
 const { verifyAdmin } = require('../middlewares/verifyAdmin');
 
 const route = express.Router();
 
+route.get('/orders', readOrders);
 route.use(verifyAccessToken);
 route.use(verifyAdmin);
 
 // ? admin request
-route.get('/revenue', readRevenue);
-route.get('/potential_revenue', readPotentialRevenue);
+route.get('/revenue/:time?', readRevenue);
+route.get('/potential_revenue/:time?', readPotentialRevenue);
+// route.get('/failed', readPotentialRevenue);
 
 module.exports = route;
