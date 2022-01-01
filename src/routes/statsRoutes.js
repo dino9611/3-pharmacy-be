d1980b45a2c6cc862d02c7926ad74d7d4850c3cc
rachmann0 <reecedarby1889@gmail.com>
2022-01-01 08:22:34 +0700

    readRevenue handle potential revenue

--- src/routes/statsRoutes.js
+++ src/routes/statsRoutes.js
@@ -1,10 +1,5 @@
 const express = require('express');
-const {
-  readRevenue,
-  readPotentialRevenue,
-  readCartedItem,
-  readSalesSuccessRate,
-} = require('../controllers/statsControllers');
+const { readRevenue } = require('../controllers/statsControllers');
 const { verifyAccessToken } = require('../helpers/verifyToken');
 const { verifyAdmin } = require('../middlewares/verifyAdmin');
 
@@ -14,9 +9,6 @@ route.use(verifyAccessToken);
 route.use(verifyAdmin);
 
 // ? admin request
-route.get('/revenue/:time?', readRevenue);
-route.get('/potential_revenue/:time?', readPotentialRevenue);
-route.get('/carted_item', readCartedItem);
-route.get('/sales_success_rate', readSalesSuccessRate);
+route.get('/revenue', readRevenue);
 
 module.exports = route;
