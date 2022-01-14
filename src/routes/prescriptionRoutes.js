543f48b31da4e15159e5c1d56ab577b080e65a59
rachmann0 <reecedarby1889@gmail.com>
2022-01-14 15:55:18 +0700

    prescription routes

--- src/routes/prescriptionRoutes.js
+++ src/routes/prescriptionRoutes.js
@@ -1,28 +1,44 @@
-const express = require("express")
-const router = express.Router()
-const { prescriptionControllers } = require("../controllers")
-const { customUpload, getDataCustom, createPrescription, updateStatus, updatePrescriptionName, getDetails, getMedicineName, getUserCustom, paymentProof } = prescriptionControllers
-const uploader = require('../helpers/uploader')
+const express = require('express');
+const router = express.Router();
+const { prescriptionControllers } = require('../controllers');
+const {
+  customUpload,
+  getDataCustom,
+  createPrescription,
+  updateStatus,
+  updatePrescriptionName,
+  getDetails,
+  getMedicineName,
+  getUserCustom,
+  paymentProof,
+} = prescriptionControllers;
+const { verifyAccessToken } = require('../helpers/verifyToken');
+const { verifyAdmin } = require('../middlewares/verifyAdmin');
+
+const uploader = require('../helpers/uploader');
 
 const customUploader = uploader('/prescription', 'CUST').fields([
-    {name: 'custom', maxCount: 3}
-])
+  { name: 'custom', maxCount: 3 },
+]);
 const paymentUploader = uploader('/paymentproof', 'PAYM').fields([
-    {name: 'proof', maxCount: 3}
-])
+  { name: 'proof', maxCount: 3 },
+]);
+
+router.use(verifyAccessToken);
 
 //! route user
-router.get("/usercustom", getUserCustom)
-router.patch("/update/payment/:id", paymentUploader, paymentProof)
+router.get('/usercustom', getUserCustom);
+router.patch('/update/payment/:id', paymentUploader, paymentProof);
+
+router.use(verifyAdmin);
 
 //! Route Admin
-router.get("/", getDataCustom)
-router.get("/details/:id", getDetails)
-router.get("/medicine/:id", getMedicineName)
-router.post("/upload/:id", customUploader , customUpload)
-router.post("/create", createPrescription)
-router.patch("/nextstatus", updateStatus)
-router.patch("/custname", updatePrescriptionName)
+router.get('/', getDataCustom);
+router.get('/details/:id', getDetails);
+router.get('/medicine/:id', getMedicineName);
+router.post('/upload/:id', customUploader, customUpload);
+router.post('/create', verifyAccessToken, verifyAdmin, createPrescription);
+router.patch('/nextstatus', updateStatus);
+router.patch('/custname', updatePrescriptionName);
 
-
-module.exports = router
+module.exports = router;
