ef8272c8c2d1e1e3b277cc4fb844ee0b48d81cb3
rachmann0 <reecedarby1889@gmail.com>
2022-01-18 13:55:53 +0700

    generateData

--- sql/generateData.js
+++ sql/generateData.js
@@ -3,12 +3,12 @@ const rand = (array) => Math.floor(Math.random() * array.length);
 const generateOrder = (n) => {
   const status = ['paymentAcc', 'paymentRej'];
   let out =
-    'INSERT INTO 3_pharmacy.order(totalPriceRp, checkedOutAt, status, user_id, profitRp) VALUES';
+    'INSERT INTO 3_pharmacy.order(totalPrice, checkedOutAt, status, user_id, profitRp) VALUES';
   for (let i = 0; i < status.length; i++) {
     for (let j = 0; j < n; j++) {
-      out += `(${Math.random() * 1000}, NOW() - INTERVAL ${j} HOUR, '${
-        status[rand(status)]
-      }', 1, ${Math.random() * 1000})`;
+      out += `(${Math.random() * 1000}, NOW() - INTERVAL ${j} DAY, '${
+        status[i]
+      }', 2, ${Math.random() * 1000})`;
       if (j !== n - 1 || i !== status.length - 1) out += ', ';
     }
   }
@@ -24,7 +24,7 @@ const generatePrescription = (n) => {
     for (let j = 0; j < n; j++) {
       out += `('name', 'img', NOW() - INTERVAL ${j} DAY, ${
         Math.random() * 1000
-      }, ${Math.random() * 1000}, '${status[i]}', 1)`;
+      }, ${Math.random() * 1000}, '${status[i]}', 2)`;
       if (j !== n - 1 || i !== status.length - 1) out += ', ';
     }
   }
@@ -32,5 +32,5 @@ const generatePrescription = (n) => {
   return out;
 };
 
-console.log(generatePrescription(500));
-console.log(generateOrder(300));
+// console.log(generatePrescription(400));
+// console.log(generateOrder(400));
